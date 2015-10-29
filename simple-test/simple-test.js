8d99ff1ed3cf7ebb94c94fe9c924ec90abb6ba90
Ken Wilcox <wilcoxk@gmail.com>
2015-10-28 23:21:22 -0600

    body.helpers - sheesh

--- simple-test/simple-test.js
+++ simple-test/simple-test.js
@@ -4,21 +4,20 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
 
+  Template.body.helpers({
+    menu: function () {
+      var items = Menu.find({});
+      console.log(items);
+      return items;
+    }
+  });
+
   Template.hello.helpers({
     counter: function () {
       var counter = Session.get('counter');
       console.log(counter);
       return counter;
-    },
-    // menu: function () {
-    //   var items = Menu.find({});
-    //   console.log(items);
-    //   return items;
-    // }
-    menu: [
-      {name: "one", price: 3.00},
-      {name: "two", price: 4.00}
-    ]
+    }
   });
 
   Template.hello.events({
